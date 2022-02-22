f0c7a81e23a038af97316058f94bf11546a191fd
Nic Peterson <nic@peterson.to>
2022-02-21 19:48:50 -0800

    Start formatting the UI.

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -14,23 +14,93 @@ export default class UI {
     const logo = document.createElement("div");
     logo.className = "logo";
     logo.id = "logo";
+    logo.innerHTML = `<span class="material-icons">
+    done_all
+    </span>ToDo!`;
 
     header.appendChild(logo);
     document.body.appendChild(header);
   }
 
   createBody() {
+    // Body => parent
     const body = document.createElement("div");
     body.className = "body";
 
+    // Sidebar => child
     const sidebar = document.createElement("div");
     sidebar.className = "sidebar";
     sidebar.id = "sidebar";
+
+    // Sidebar children
+    // First sidebar child
+    const defaultProjectList = document.createElement("div");
+
+    // first sidebar grandchild
+    const inbox = document.createElement("button");
+    inbox.class = "button-default-project active";
+    inbox.id = "button-inbox-projects";
+    inbox.innerHTML = `<span class="material-icons">
+    inbox
+    </span>Inbox
+    `;
+    // second sidebar grandchild
+    const today = document.createElement("button");
+    today.class = "button-default-project active";
+    today.id = "button-today-projects";
+    today.innerHTML = `<span class="material-icons">
+    today
+    </span>Today
+    `;
+
+    // third sidebar grandchild
+    const week = document.createElement("button");
+    week.class = "button-default-project active";
+    week.id = "button-week-projects";
+    week.innerHTML = `<span class="material-icons">
+    date_range
+    </span>Week
+    `;
+
+    // Appending sidebar grandchild
+    defaultProjectList.appendChild(inbox);
+    defaultProjectList.appendChild(today);
+    defaultProjectList.appendChild(week);
+    sidebar.appendChild(defaultProjectList);
+
+    // Appending second sidebar child
+    const projectsTitle = document.createElement("div");
+    projectsTitle.className = "projects-title";
+    projectsTitle.textContent = "Projects";
+    sidebar.appendChild(projectsTitle);
+
+    
+
+    // Appending third sidebar child
+    const projectsList = document.createElement("div");
+    //const addProject
+
+
+    sidebar.appendChild(projectsList);
+
+    // Apending fourth sidebar child
+    const addProjectBtn = document.createElement("button")
+    addProjectBtn.class = "button-add-project";
+    addProjectBtn.id = "button-add-project";
+    addProjectBtn.innerHTML = `<span class="material-icons">
+    add
+    </span>Add Project`
+
+
+    sidebar.appendChild(addProjectBtn);
 
     const container = document.createElement("div");
     container.className = "container";
     container.id = "container";
 
+    //Appending body children
+    body.appendChild(sidebar);
+    body.appendChild(container);
     document.body.appendChild(body);
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
     index: "./src/index.js",
+    vendors: ["webpack-material-design-icons"],
   },
   devtool: "inline-source-map",
   devServer: {
@@ -27,6 +28,12 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+    ],
+    loaders: [
+      {
+        test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/,
+        loader: "file?name=[name].[ext]",
       },
     ],
   },
