dd908d76801b156a7d0a2404a4e1bbc3076bc4c4
Nic Peterson <nic@peterson.to>
2022-02-24 18:45:54 -0800

    Refactored all UI items to be class properties so the methods can be accessed anywhere. Set constructor and updated createHeader and createBody.

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,5 +1,5 @@
 import { add } from "date-fns";
-import { createElt, addListener } from "./dom";
+import { createElt, addListener, returnNodeById } from "./dom";
 import Project from "./project";
 import Task from "./task";
 import {
@@ -11,8 +11,85 @@ import {
 } from "./localStorage";
 
 export default class UI {
+  constructor() {
+    this.header = createElt(document.body, "div", "header", "header", "");
+    this.logo = createElt(
+      this.header,
+      "div",
+      "logo",
+      "logo",
+      `<p><span class="material-icons">
+    done_all
+    </span>ToDo!</p>`
+    );
+    this.body = createElt(document.body, "div", "body", "body", "");
+    this.sidebar = createElt(this.body, "div", "sidebar", "sidebar", "");
+    this.defaultProjectList = createElt(
+      sidebar,
+      "div",
+      "default-project-list",
+      "default-project-list",
+      ""
+    );
+    this.inbox = createElt(
+      this.defaultProjectList,
+      "button",
+      "button-default-project",
+      "button-inbox-projects",
+      `<p><span class="material-icons">
+    inbox
+    </span>Inbox</p>
+    ` 
+    );
+    this.today = createElt(
+      this.defaultProjectList,
+      "button",
+      "button-default-project",
+      "button-today-project",
+      `<p><span class="material-icons">
+    today
+    </span>Today</p>
+    `
+    );
+    this.week = createElt(
+      this.defaultProjectList,
+      "button",
+      "button-default-project",
+      "button-week-projects",
+      `<p><span class="material-icons">
+    date_range
+    </span>Week</p>
+    `);
+    this.projectsTitle = createElt(
+      this.sidebar,
+      "div",
+      "projects-title",
+      "projects-title",
+      "Projects"
+    );
+    this.projectList = createElt(
+      this.sidebar,
+      "div",
+      "projects-list",
+      "projects-list",
+      ""
+    );
+    this.addProjectButton = this.createAddProjectButton(this.sidebar);
+    this.container = createElt(this.body, "div", "container", "container", "");
+    this.projectname = createElt(
+      this.container,
+      "div",
+      "project-name",
+      "project-name",
+      "<h1>Inbox</h1>"
+    );
+    this.taskList = createElt(this.container, "div", "task-list", "task-list", "");
+    this.taskArr = this.retrieveTasks();
+    this.addTaskButton = this.createAddTaskButton(this.container);
+  }
+
   loadPage() {
-    this.createHeader();
+    //this.createHeader();
     this.createBody();
     this.createFooter();
     this.retrieveTasks();
@@ -21,29 +98,30 @@ export default class UI {
   }
 
   createHeader() {
-    const header = createElt(document.body, "div", "header", "header", "");
+    // const header = createElt(document.body, "div", "header", "header", "");
 
-    const logo = createElt(
-      header,
-      "div",
-      "logo",
-      "logo",
-      `<p><span class="material-icons">
-    done_all
-    </span>ToDo!</p>`
-    );
+    // const logo = createElt(
+    //   header,
+    //   "div",
+    //   "logo",
+    //   "logo",
+    //   `<p><span class="material-icons">
+    // done_all
+    // </span>ToDo!</p>`
+    // );
   }
 
   createBody() {
     // Body. This the parent container. It has two children: sidebar (listing out projects) and body (listing out tasks)
-    const body = createElt(document.body, "div", "body", "body", "");
+    //const body = createElt(document.body, "div", "body", "body", "");
 
     // Sidebar → Body child #1.
-    const sidebar = createElt(body, "div", "sidebar", "sidebar", "");
+    //const sidebar = createElt(body, "div", "sidebar", "sidebar", "");
 
     // Sidebar children
 
     // First sidebar child. Breaks down the default projects: inbox, due today, and due this week
+    /*
     const defaultProjectList = createElt(
       sidebar,
       "div",
@@ -51,8 +129,10 @@ export default class UI {
       "default-project-list",
       ""
     );
+    */
     // sidebar grandchildren
     // All purpose inbox for capturing default tasks
+    /*
     const inbox = createElt(
       defaultProjectList,
       "button",
@@ -63,7 +143,9 @@ export default class UI {
     </span>Inbox</p>
     `
     );
+    */
     // Shows tasks due today
+    /*
     const today = createElt(
       defaultProjectList,
       "button",
@@ -74,7 +156,9 @@ export default class UI {
     </span>Today</p>
     `
     );
+    */
     // Shows tasks due this week
+    /*
     const week = createElt(
       defaultProjectList,
       "button",
@@ -85,8 +169,9 @@ export default class UI {
     </span>Week</p>
     `
     );
-
+      */
     // Second sidebar child - PROJECT. We'll give the user the ability to add/view custom projects below this title
+    /*
     const projectsTitle = createElt(
       sidebar,
       "div",
@@ -94,8 +179,10 @@ export default class UI {
       "projects-title",
       "Projects"
     );
+    */
 
     // Appending third sidebar child. This element is a container for the users' list of custom projects
+    /*
     const projectList = createElt(
       sidebar,
       "div",
@@ -103,15 +190,17 @@ export default class UI {
       "projects-list",
       ""
     );
+    */
 
     // Apending fourth sidebar child. The user clicks this button to create new custom projects.
-    const addProjectButton = this.createAddProjectButton(sidebar);
+    //const addProjectButton = this.createAddProjectButton(this.sidebar);
 
     // container → Body child #2
-    const container = createElt(body, "div", "container", "container", "");
+    //const container = createElt(body, "div", "container", "container", "");
 
     // container child #1. Shows current project name
     // TODO → will need to make this dynamic based on the project selected
+    /*
     const projectname = createElt(
       container,
       "div",
@@ -119,16 +208,17 @@ export default class UI {
       "project-name",
       "<h1>Inbox</h1>"
     );
+    */
 
     // container child #2. Shows the tasks list
     // TODO make this dynamic
-    const taskList = createElt(container, "div", "task-list", "task-list", "");
-    const taskArr = this.retrieveTasks();
-    this.renderTaskList(taskList, taskArr);
+    //const taskList = createElt(container, "div", "task-list", "task-list", "");
+    //const taskArr = this.retrieveTasks();
+    this.renderTaskList(this.taskList, this.taskArr);
 
     // container child
     // TODO after the task button is clicked, create a modal that adds the task
-    const addTaskButton = this.createAddTaskButton(container);
+    //const addTaskButton = this.createAddTaskButton(container);
   }
 
   createFooter() {
@@ -211,6 +301,8 @@ export default class UI {
     console.log(projectTasks);
   }
 
+  clear(parentNode) {}
+
   renderTaskList(parentNode, tasks) {
     if (tasks.length < 1) {
       return;
@@ -222,9 +314,8 @@ export default class UI {
           "task",
           elt.id,
           `<div class="task-left">
-          <div class="radio ${elt.priority}">
+          <div class="radio ${elt.priority}" id="complete-task-${elt.id}">
             <span class="material-icons"> radio_button_unchecked </span>
-            <span class="material-icons"> radio_button_checked </span>
           </div>
           <div class="task-basic-info">
             <div class="task-title">${elt.title}</div>
@@ -232,12 +323,23 @@ export default class UI {
           </div>
         </div>
         <div class="task-right">
-          <div class="edit-task">
+          <div class="edit-task" id="edit-task-${elt.id}">
             <span class="material-icons"> edit </span>
           </div>
+          <div class="delete-task" id="delete-task-${elt.id}">
+            <span class="material-icons"> close </span>
         </div>
           `
         );
+        addListener(returnNodeById(`complete-task-${elt.id}`), "click", () => {
+          console.log(`complete-task-${elt.id}`);
+        });
+        addListener(returnNodeById(`edit-task-${elt.id}`), "click", () => {
+          console.log(`edit-task-${elt.id}`);
+        });
+        addListener(returnNodeById(`delete-task-${elt.id}`), "click", () => {
+          console.log(`delete-task-${elt.id}`);
+        });
       });
     }
   }
